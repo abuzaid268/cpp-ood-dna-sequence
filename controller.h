9c73b760baabd82408d25cad2f724e9195de1575
abuzaid268 <mhalaweh2006@gmail.com>
2020-03-17 06:48:24 -0700

    added controller

---
+++ controller.h
@@ -0,0 +1,16 @@
+//
+// Created by abuzaid268 on 3/17/20.
+//
+
+#ifndef DNA_SEQUENCE_C___CONTROLLER_H
+#define DNA_SEQUENCE_C___CONTROLLER_H
+
+
+struct controller {
+    controller(){}
+
+    void runDnaAnalyzer();
+};
+
+
+#endif //DNA_SEQUENCE_C___CONTROLLER_H
--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
-#include <iostream>
-#include "parser.h"
-#include "factory.h"
-#include "command.h"
-#include "DNAExceptions.h"
-#include <memory>
+#include "controller.h"
 using namespace std;
 
 int main() {
-    std::string input;
-    vector<string> inputs;
-    command *cmd = NULL;
-    while (input != "quit") {
-        cout << "> cmd >>> ";
-        getline(cin, input);
-        if (input != "quit") {
-            inputs = Parser::getCommand(input);
-            try {
-                cmd = (factory::method(inputs));
-                cmd->execute(inputs);
-                inputs.clear();
-            }
-            catch (exception &e) {
-                cout << e.what() << endl;
-            }
-        }
-    }
-    factory::delete_ptrs();
+    controller ctrl;
+    ctrl.runDnaAnalyzer();
 }
